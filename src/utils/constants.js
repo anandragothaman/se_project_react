5dfc8e13c8634976082043726444cd24a30e9de8
Anand Ragothaman <r.anandragothaman@gmail.com>
2025-07-08 17:34:06 -0500

    feat: add weatherdataapi

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -36,3 +36,10 @@ export const defaultClothingItems = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 ];
+
+export const coordinates = {
+  latitude: 35.995548,
+  longitude: -86.778893,
+};
+
+export const APIkey = "da30ec525032febe6df357eb1c7b9ae1";
